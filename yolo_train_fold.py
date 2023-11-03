886b7315dbf5bf4c7c03ac64aa1e85715fc5be9f
huypl53 <phamlehuy52@gmail.com>
2023-11-03 20:25:17 +0700

    yolo project on init

--- yolo_train_fold.py
+++ yolo_train_fold.py
@@ -9,7 +9,6 @@ results = {}
 ksplit=YOLO_K_SPLIT
 # Define your additional arguments here
 batch = 16
-project = 'kfold_demo'
 epochs = 200
 
 model = YOLO('yolov8m.pt')
@@ -26,6 +25,8 @@ except:
 ds_yamls = open(fold_list_file, 'r').read().split('\n')
 for k in range(ksplit):
     dataset_yaml = ds_yamls[k]
+    dataset_base_dir = os.path.dirname( dataset_yaml.rsplit(YOLO_K_FOLD_DIR, 1)[0] )
+    project = os.path.join('k-fold-runs', os.path.basename(dataset_base_dir) )
     model.train(data=dataset_yaml,epochs=epochs, batch=batch, project=project)  # include any train arguments
     results[k] = model.metrics  # save output metrics for further analysis
 
