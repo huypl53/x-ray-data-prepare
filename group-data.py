678d027bb8f0154b1c770fff05f6762c8e915927
huypl53 <phamlehuy52@gmail.com>
2023-12-19 10:23:09 +0700

    update grouping

--- group-data.py
+++ group-data.py
@@ -14,13 +14,15 @@ _root_dataset_dir = 'datasets'
 def change_label(labels, new_class_id):
     new_labels = []
     for label in labels:
+        if not len(label): 
+            continue
         label[0] = int(new_class_id)
         new_labels.append(label)
     return new_labels
 
 if __name__ == '__main__':
     input_dirs = sys.argv[1: -1]
-    assert len(input_dirs) < 2
+    assert len(input_dirs) > 1
     output_dir = sys.argv[-1]
 
 
@@ -32,35 +34,32 @@ if __name__ == '__main__':
     for task_dir in task_dirs:
         for t in ims_lbs:
             for mode_dir in mode_dirs:
-
-                (output_path / task_dir / t / mode_dir).mkdir()
-        
+                (output_path / task_dir / t / mode_dir).mkdir(parents=True, exist_ok=True)
 
         sub_data_names = []
         sub_data_labels = []
         for i, input_dir in enumerate( input_dirs ):
-            bname = os.path.basename(input_dir)
+            bname = Path(input_dir).name
             sub_data_names.append(bname)
             sub_data_labels.append(f'{i}: {bname}')
 
-        sub_data_labels_str = '\n'.join(sub_data_labels)
+        sub_data_labels_str = '\n    '.join(sub_data_labels)
         new_ds_name = '_'.join(sub_data_names)
         open(os.path.join( f"{_root_dataset_dir}", f"{new_ds_name}-{'bbox' if task_dir == YOLO_DIR else 'seg'}-coco.yaml" ), 'w', encoding='utf-8' ).write(
     f"""
-    path: {str(output_path/task_dir)}  # dataset root dir
-    train: images/train  # train images (relative to 'path') 4 images
-    val: images/val  # val images (relative to 'path') 4 images
-    test:  # test images (optional)
+path: {str(output_path/task_dir)}  # dataset root dir
+train: images/train  # train images (relative to 'path') 4 images
+val: images/val  # val images (relative to 'path') 4 images
+test:  # test images (optional)
 
 # Classes (80 COCO classes)
-    names:
+names:
     {sub_data_labels_str}
     """
         )
 
-
     for i, input_dir in enumerate( tqdm( input_dirs, leave=False ) ):
-        bname = os.path.basename(input_dir)
+        bname = Path(input_dir).name
         for task_dir in task_dirs:
             for t in ims_lbs:
                 for mode_dir in mode_dirs:
@@ -78,8 +77,8 @@ if __name__ == '__main__':
                         for label_file in tqdm(label_files, leave=False):
                             basename = os.path.basename(label_file)
                             labels = parse_label_file(label_file)
-                            labels = change_label(labels, i)
-                            label_str = label_num2str(labels) 
-                            save_labels( label_str, os.path.join(dest_dir, basename))
+                            if (len(labels)):
+                                labels = change_label(labels, i)
+                            save_labels( labels, os.path.join(dest_dir, basename))
 
 
