6f5d2a164c231fc6c2d7f7e5a549b9ec8e0f3f12
huypl53 <phamlehuy53@gmail.com>
2024-01-31 07:50:20 +0700

    remove small bboxes update empty label file

--- remove_small_bbox.py
+++ remove_small_bbox.py
@@ -15,13 +15,17 @@ if __name__ == '__main__':
     in_files = glob(f'{in_dir}/*.*')
     for file_path in tqdm( in_files, position=0, leave=False ):
         bboxes = parse_label_file(file_path)
-        new_bboxes = []
-        for box in tqdm( bboxes, position=1 ):
-            min_d = min(box[3], box[4])
-            if min_d < 0.01:
-                continue
-            new_bboxes.append(box)
-
         basename = os.path.basename(file_path)
+        new_bboxes = []
+        try:
+            for box in bboxes:
+                min_d = min(box[3], box[4])
+                if min_d < 0.05:
+                    continue
+                new_bboxes.append(box)
+        except Exception as e: 
+            print(f'Error {e} at {file_path}')
+            new_bboxes = bboxes
+
         out_path = os.path.join(out_dir, basename)
         save_labels(new_bboxes, out_path)
